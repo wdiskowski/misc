877b63033dddb318dab5a961143712f9f700ea78
Wladimir Diskowski <wdiskowski@gmail.com>
2015-02-15 22:11:50 +0100

    java-8 Date

--- src/test/java/org/wd/example/date/DateUtilsTest.java
+++ src/test/java/org/wd/example/date/DateUtilsTest.java
@@ -1,15 +1,51 @@
 package org.wd.example.date;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 import java.time.Instant;
-import java.time.temporal.ChronoField;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
 public class DateUtilsTest {
 
+	private static final String GERMAN_DATE_FORMAT = "dd.MM.yyyy";
+	private static final Instant FIRST_INSTANT = Instant.now();
+	private static final long SOME_DURATION = 10L;
+
 	@Test
-	public void testName() {
-		Instant instant = Instant.now();
-		System.out.println(instant.get(ChronoField.MILLI_OF_SECOND));
+	public void isAfterPositiveTest() {
+		Instant secondInstant = FIRST_INSTANT.plusSeconds(SOME_DURATION);
+		assertThat(DateUtils.isAfter(secondInstant, FIRST_INSTANT),
+				equalTo(true));
 	}
+
+	@Test
+	public void isAfterNegativeTest() {
+		Instant secondInstant = FIRST_INSTANT.plusSeconds(-SOME_DURATION);
+		assertThat(DateUtils.isAfter(secondInstant, FIRST_INSTANT),
+				equalTo(false));
+	}
+
+	@Test
+	public void getDiferenceSecondsTest() {
+		Instant secondInstant = FIRST_INSTANT.plusSeconds(SOME_DURATION);
+		assertThat(DateUtils.getDiference(secondInstant, FIRST_INSTANT,
+				ChronoUnit.SECONDS), equalTo(SOME_DURATION));
+	}
+
+	@Test
+	public void formatTest() {
+		LocalDate date = LocalDate.now();
+		String sollFormatedDate = String.format("%02d.%02d.%04d",
+				date.getDayOfMonth(), date.getMonth().getValue(),
+				date.getYear());
+		assertThat(DateUtils.format(date, GERMAN_DATE_FORMAT),
+				equalTo(sollFormatedDate));
+	}
+
 }
