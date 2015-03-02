b7a2269d373c29e0323cbd094c6013f121573ab0
Wladimir Diskowski <wdiskowski@gmail.com>
2015-03-02 21:06:17 +0100

    BookService erweitert

--- src/main/java/org/wd/example/lambdas/BookService.java
+++ src/main/java/org/wd/example/lambdas/BookService.java
@@ -1,6 +1,8 @@
 package org.wd.example.lambdas;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.wd.example.lambdas.Book.Topic;
@@ -22,6 +24,45 @@ public class BookService {
 		return bookRepository.getAll().parallelStream()
 				.filter(b -> b.getTopic() == topic)
 				.collect(Collectors.toList());
+	}
+
+	public List<String> getTitles() {
+		return bookRepository.getAll().parallelStream().map(Book::getTitle)
+				.collect(Collectors.toList());
+	}
+
+	public List<Book> getSorted() {
+		return bookRepository.getAll().parallelStream()
+				.sorted(Comparator.comparing(Book::getTitle))
+				.collect(Collectors.toList());
+	}
+
+	public List<String> getAuthors() {
+		return bookRepository.getAll().parallelStream()
+				.flatMap(b -> b.getAuthors().stream()).distinct()
+				.collect(Collectors.toList());
+	}
+
+	public List<Book> getSortedLimited(int limit) {
+		return bookRepository.getAll().parallelStream()
+				.sorted(Comparator.comparing(Book::getTitle)).limit(limit)
+				.collect(Collectors.toList());
+	}
+
+	public List<Book> getSortedRest(int skiped) {
+		return bookRepository.getAll().parallelStream()
+				.sorted(Comparator.comparing(Book::getTitle)).skip(skiped)
+				.collect(Collectors.toList());
+	}
+
+	public Book getOldest() {
+		return bookRepository.getAll().parallelStream()
+				.min(Comparator.comparing(Book::getPubDate)).get();
+	}
+
+	public Map<Topic, List<Book>> getAsMapByTopic() {
+		return bookRepository.getAll().parallelStream()
+				.collect(Collectors.groupingBy(Book::getTopic));
 	}
 
 }
--- src/test/java/org/wd/example/lambdas/BookServiceTest.java
+++ src/test/java/org/wd/example/lambdas/BookServiceTest.java
@@ -2,10 +2,13 @@ package org.wd.example.lambdas;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import java.util.Collections;
+
 import java.util.List;
-import org.wd.example.lambdas.Book.Topic;
+import java.util.Map;
+import java.util.Random;
+
 import org.junit.Test;
+import org.wd.example.lambdas.Book.Topic;
 
 /**
  * Tests for {@link BookService}
@@ -27,5 +30,75 @@ public class BookServiceTest {
 		boolean onlyFictions = fictionBooks.stream().allMatch(
 				b -> b.getTopic() == Topic.FICTION);
 		assertThat(onlyFictions, equalTo(true));
+	}
+
+	@Test
+	public void getTitlesTest() {
+		List<String> titles = bookService.getTitles();
+		assertThat(
+				titles,
+				hasItems(BookRepository.COMPILERS.getTitle(),
+						BookRepository.NAILS.getTitle()));
+	}
+
+	@Test
+	public void getSortedTest() {
+		List<Book> books = bookService.getSorted();
+		for (int i = 0; i < books.size(); i++) {
+			Book refBook = books.get(i);
+			for (int j = i + 1; j < books.size(); j++) {
+				assertThat(refBook.getTitle(), lessThanOrEqualTo(books.get(j)
+						.getTitle()));
+			}
+		}
+	}
+
+	@Test
+	public void getAuthorsTest() {
+		List<String> authors = bookService.getAuthors();
+		assertThat(
+				authors,
+				hasItems(BookRepository.COMPILERS.getAuthors()
+						.toArray(
+								new String[BookRepository.COMPILERS
+										.getAuthors().size()])));
+	}
+
+	@Test
+	public void getSortedLimitedTest() {
+		List<Book> booksAll = bookService.getSorted();
+		int limit = new Random().nextInt(booksAll.size());
+		List<Book> booksLimited = bookService.getSortedLimited(limit);
+		assertThat(booksLimited, hasSize(limit));
+		for (int i = 0; i < limit; i++) {
+			assertThat(booksLimited.get(i), equalTo(booksAll.get(i)));
+		}
+	}
+
+	@Test
+	public void getSortedRestTest() {
+		List<Book> booksAll = bookService.getSorted();
+		int skiped = new Random().nextInt(booksAll.size());
+		List<Book> booksRest = bookService.getSortedRest(skiped);
+		assertThat(booksRest, hasSize(booksAll.size() - skiped));
+		for (int i = 0; i < skiped; i++) {
+			assertThat(booksRest.get(i), equalTo(booksAll.get(skiped + i)));
+		}
+	}
+
+	@Test
+	public void getOldestTest() {
+		List<Book> booksAll = bookService.getSorted();
+		Book bookOldest = bookService.getOldest();
+		boolean oldest = booksAll.parallelStream().allMatch(
+				b -> !b.getPubDate().isBefore(bookOldest.getPubDate()));
+		assertThat(oldest, equalTo(true));
+	}
+
+	@Test
+	public void getAsMapByTopicTest() {
+		Map<Book.Topic, List<Book>> mapByTopic = bookService.getAsMapByTopic();
+		assertThat(mapByTopic.get(BookRepository.NAILS.getTopic()),
+				hasItem(BookRepository.NAILS));
 	}
 }
